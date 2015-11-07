2605841d464f2131fbcf3713ee21972fe33b6298
Chris Lyon <flushot@gmail.com>
2015-11-07 00:59:14 -0800

    updates

--- client.py
+++ client.py
@@ -20,7 +20,12 @@ if __name__ == '__main__':
 
     sck.setsockopt_string(zmq.SUBSCRIBE, filter_str)
 
+    last = []
     while True:
         message = sck.recv_string()
         i, num = message.split(' ', 1)
-        log.debug('%s -> %s' % (i, num))
+        last.append(int(num[:-1]))  # remove null terminator
+        if len(last) == 10**4:
+            average = reduce(lambda x, y: x + y, last, 0) / len(last)
+            log.debug(average)
+            last = []
--- cserver/server.c
+++ cserver/server.c
@@ -4,11 +4,12 @@
 #include <assert.h>
 #include <sys/time.h>
 #include <signal.h>
+#include <locale.h>
 #include <zmq.h>
 
 int is_running = 1;
 
-void handle_sigint(int signum) {
+void die_signal_handler(int signum) {
     is_running = 0;
 }
 
@@ -16,7 +17,13 @@ int main(int argc, char **argv) {
     int rc;
     unsigned i, randn;
     char message[100];
-    void *ctx, *sck;
+    void *ctx, *sck;  /* zmq socket */
+	clock_t begin, end;  /* perf timing */
+	double elapsed;
+	int msg_ctr = 0;
+
+	/* printf number formatting with commas */
+	setlocale(LC_NUMERIC, "");
 
     ctx = zmq_ctx_new();
     assert(ctx);
@@ -31,18 +38,29 @@ int main(int argc, char **argv) {
     }
 
     srand(time(NULL));
-    signal(SIGINT, handle_sigint);
+    signal(SIGINT, die_signal_handler);
+    signal(SIGTERM, die_signal_handler);
 
+	begin = clock();
     while (is_running) {
       randn = rand() * 2 ^ (8 * sizeof(randn));
       sprintf(message, "%d %u", i++ % 30, randn);
-      printf("%s\n", message);
 
       rc = zmq_send(sck, &message, strlen(message) + 1, 0);
       if (rc == -1) {
         perror("zmq_send()");
         is_running = 0;
       }
+
+	  ++msg_ctr;
+
+	  end = clock();
+	  elapsed = (double)(end - begin) / CLOCKS_PER_SEC;
+	  if (elapsed >= 1.0) {
+		printf("Published: %'d messages/sec\n", msg_ctr);
+		msg_ctr = 0;
+		begin = clock();
+	  }
     }
 
 cleanup:
